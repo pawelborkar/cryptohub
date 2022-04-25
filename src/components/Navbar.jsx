36c3c5b1307a0701cbeaa12817c36d217f769238
Pawel Borkar <pawelsb@zohomail.com>
2022-04-25 19:21:52 +0530

    Clean up

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,6 @@ import { Button, Menu, Typography, Avatar } from "antd";
 import { Link } from "react-router-dom";
 import {
     HomeOutlined,
-    MoneyCollectOutlined,
     FundOutlined,
     BulbOutlined,
     MenuOutlined,
